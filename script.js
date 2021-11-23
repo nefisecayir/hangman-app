b131cd445df461cb20ceb32435a3b4c79e010efa
Nefise Çayır <pinkuugg@gmail.com>
2021-11-23 11:52:00 +0300

    updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const word_el = document.getElementById('words');
 const correctLetters = ["r", "e", "h", "g", "t"]
 const wrongLetters = [];
+const popup = document.getElementById('popup-container');
+const msg_el = document.getElementById('succes-message');
 
 function getRandomWord() {
     const words = ["congrats", "youre", "the", "murderer"];
@@ -11,23 +13,30 @@ function getRandomWord() {
 
 //console.log(getRandomWord());
 
-function displayWord() {
+const displayWord = () =>  {
     const selectedWord = getRandomWord();
 
-    word_el.innerHTML = `
-        ${selectedWord.split('').map(letter => `
+    word_el.innerHTML = ` 
+        ${selectedWord.split('').map((letter) => `
             <div class="letter">
                 ${correctLetters.includes(letter) ? letter: ''}
+                    
             </div>
         `).join('')}
     `;
+
+    //console.log(word_el.innerText);
+
     const w = word_el.innerText.replace(/\n/g,'');
     if (w === selectedWord) {
-        console.log('you found it.')
+        //console.log('you found it.')
+        popup.style.display = 'flex';
+        msg_el.innerText = 'YOU KILLED HIM'
     }
     
-    
     //console.log(word_el.innerText.replace(/\n/g,''));
+    
+    
 }
 
 displayWord();
